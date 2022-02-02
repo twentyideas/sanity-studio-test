3b88e2f359c2573609cd0e7e928d1a9cde10cadc
Greg McKelvey <mckelveygreg@gmail.com>
2022-02-02 12:59:11 -0800

    add local storage peristence

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { createWebStoragePersistor } from "react-query/createWebStoragePersistor-experimental"
+import { persistQueryClient } from "react-query/persistQueryClient-experimental"
 import App from './App'
 import './index.css'
 
-const client = new QueryClient()
 
+ const queryClient = new QueryClient({
+   defaultOptions: {
+     queries: {
+       cacheTime: 1000 * 60 * 60 * 24, // 24 hours
+     },
+   },
+ });
+
+ const localStoragePersistor = createWebStoragePersistor({
+   storage: window.localStorage,
+ });
+
+ persistQueryClient({
+   queryClient,
+   persistor: localStoragePersistor,
+ });
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={client}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
